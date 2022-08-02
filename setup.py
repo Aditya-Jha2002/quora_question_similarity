47b388bca718b7daa99deaf845565bd6b615a216
aditya_jha <4adityajha@gmail.com>
2022-08-02 22:17:52 +0530

    initial commit and added initial data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description="Creates a Machine Learning and Natural Language Processing based model, and serves it up via FastA. The API takes 2 questions as input, and returns it's similarity score",
+    author='Aditya Jha',
+    license='MIT',
+)
